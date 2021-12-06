38adbc37177436ff984bf96c80952d5869f1ee94
Jang HyungSuk <jeremy0405@naver.com>
2021-12-06 15:08:10 +0900

    지도 데이터를 갖고 있는 StageData 클래스 생성
    한 일
     - txt파일이 아닌 문자열을 가지고 있는 StageData 클래스 생성

---
+++ StageData.java
@@ -0,0 +1,19 @@
+public class StageData {
+
+    public static String[] mapdata = {"Stage 1",
+    "#####",
+    "#OoP#",
+    "#####",
+    "=====",
+    "Stage 2",
+    "  #######  ",
+    "###  O  ###",
+    "#    o    #",
+    "# Oo P oO #",
+    "###  o  ###",
+    " #   O  #  ",
+    " ########  "};
+
+
+
+}
