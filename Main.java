116d760c0c016ed1d53ec40146b1cf799f9f0c9f
Jang HyungSuk <jeremy0405@naver.com>
2021-12-06 15:37:46 +0900

    PrintMap 클래스 생성
    한 일
     - PrintMap 클래스에 static 메서드를 이용해 출력할 수 있게 했다.
    
    todoList
     - MapReader에서 구멍의 수, 공의 수, 플레이어 위치를 가지고 있는 전역변수를 생성하고 getter를 해야 겠다.

--- Main.java
+++ Main.java
@@ -10,16 +10,7 @@ public class Main {
         for (int i = 1; i <= stageNum; i++) {
             String[][] map = mapReader.getStages(i);
 
-            //todo print하는 클래스 호출
-
-            for (int j = 0; j < map.length; j++) {
-                for (int k = 0; k < map[j].length; k++) {
-                    System.out.print(map[j][k]);
-                }
-                System.out.println("");
-            }
-            System.out.println("가로 크기" + mapReader.getWidth().get(i - 1));
-            System.out.println("세로 크기" + mapReader.getHeight().get(i - 1));
+            PrintMap.print(map, i, mapReader);
 
         }
 
---
+++ PrintMap.java
@@ -0,0 +1,36 @@
+public class PrintMap {
+
+    public static void print(String[][] map, int i, MapReader mapReader) {
+        System.out.println("Stage " + i);
+        System.out.println("");
+        for (int j = 0; j < map.length; j++) {
+            for (int k = 0; k < map[j].length; k++) {
+                switch (map[j][k]) {
+                    case "0" :
+                        System.out.print("#");
+                        break;
+                    case "1" :
+                        System.out.print("O");
+                        break;
+                    case "2" :
+                        System.out.print("o");
+                        break;
+                    case "3" :
+                        System.out.print("P");
+                        break;
+                    default:
+                        System.out.print(" ");
+                }
+            }
+            System.out.println("");
+        }
+        System.out.println("");
+        System.out.println("가로 크기: " + mapReader.getWidth().get(i - 1));
+        System.out.println("세로 크기: " + mapReader.getHeight().get(i - 1));
+        System.out.println("구멍의 수: ");
+        System.out.println("공의 수: ");
+        System.out.println("플레이어 위치 (");
+        System.out.println("");
+    }
+
+}
