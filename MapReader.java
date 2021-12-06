6ecc028bf75b61e423b7ba31b514a31f1cdfc959
Jang HyungSuk <jeremy0405@naver.com>
2021-12-06 14:06:52 +0900

    MapReader에서 Stage별로 값 읽기 구현
    한 일
     - map.txt에서 Stage별로 값 출력하도록 하는 메서드 생성
    
    할 일
     - 출력이 아닌 배열 저장하는 기능으로 변경해야 함

--- MapReader.java
+++ MapReader.java
@@ -7,13 +7,14 @@ import java.util.List;
 public class MapReader {
 
     private List<String> lines;
+
+    //stageLine에 Stage1 or Stage2 or Stage3 가 입력되어 있는 라인의 column을 저장
     private List<Integer> stageLine = new ArrayList<>();
 
     // 맵 읽는 생성자
     // 맵을 읽은 후 stage 개수를 체크함
-    public void MapReader() throws IOException {
+    MapReader() throws IOException {
         readMapFile();
-        stageLine.add(1);
         countStage();
     }
 
@@ -22,37 +23,38 @@ public class MapReader {
     }
 
     private void countStage() {
+        stageLine.add(0);
         int size = lines.size();
         for (int i = 0; i < size; i++) {
             if (lines.get(i).contains("=")) {
-                setStageLine(i, size);
+                stageLine.add(i + 1);
             }
         }
     }
 
-    private void setStageLine(int i, int size) {
-        if (i + 1 == size) {
-            return;
-        }
-        stageLine.add(i + 1);
+    public int getStageSize(){
+        return stageLine.size();
     }
 
-    public int[][] getStages() {
+    public void getStages(int stageNum) {
 
         //todo map을 생성해줘야 함.
 
+        int start;
+        int last;
 
-        for (int i = 0; i < lines.size(); i++) {
-            if (lines.get(i).contains("=")) {
-                continue;
-            }
-            String line = lines.get(i);
-
-            //todo 배열 값에 맞춰서 저장하는 메서드
-
-            System.out.println(lines.get(i));
+        if (stageNum == stageLine.size()) {
+            start = stageLine.get(stageNum - 1);
+            last = lines.size();
+        } else {
+            start = stageLine.get(stageNum - 1);
+            last = stageLine.get(stageNum) - 1;
         }
 
+        for (int i = start; i < last; i++) {
+            String line = lines.get(i);
+            System.out.println(line);
+        }
 
     }
 
