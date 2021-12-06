5255ccbd76dd808b46c4559210f79c8c910d884d
Jang HyungSuk <jeremy0405@naver.com>
2021-12-06 18:41:39 +0900

    2단계에 맞춰 PrintMap 필요 없는 부분 삭제
    한 일
     - 가로 크기, 세로 크기 등 1단계에서 필요했던 것들 모두 삭제
     - 매개변수 map 하나만 받아서 출력
    
    todoList
     - input에 따른 이동 구현
     - 이동하는 클래스 생성해야 함
     - 게임 클래스 생성 후 게임 클래스에서 이동, input 관리, mapReader 관리 해야 함

--- PrintMap.java
+++ PrintMap.java
@@ -1,7 +1,6 @@
 public class PrintMap {
 
-    public static void print(int[][] map, int i, MapReader mapReader) {
-        System.out.println("Stage " + i);
+    public static void print(int[][] map) {
         System.out.println("");
         for (int j = 0; j < map.length; j++) {
             for (int k = 0; k < map[j].length; k++) {
@@ -24,14 +23,7 @@ public class PrintMap {
             }
             System.out.println("");
         }
-        System.out.println("");
-        System.out.println("가로 크기: " + mapReader.getWidth().get(i - 1));
-        System.out.println("세로 크기: " + mapReader.getHeight().get(i - 1));
-        System.out.println("구멍의 수: " + mapReader.getHoleCount().get(i - 1));
-        System.out.println("공의 수: " + mapReader.getBallCount().get(i - 1));
-        System.out.println("플레이어 위치 (" + (mapReader.getPlayerLocation().get(i - 1).getX() + 1) + ", "
-            + (mapReader.getPlayerLocation().get(i - 1).getY() + 1) + ")");
-        System.out.println("");
+
     }
 
 }
