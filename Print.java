37a4f70d1d8ea11b486b298139b75b10e6ab67db
Jang HyungSuk <jeremy0405@naver.com>
2021-12-07 15:54:15 +0900

    5 Stage 클리어시 게임종료 출력
    한 일
     - 5 Stage 클리어시 게임종료 출력
    
    todoList
     - 리팩토링

--- Print.java
+++ Print.java
@@ -2,9 +2,9 @@ public class Print {
 
     public static void printMap(int[][] map) {
         System.out.println("");
-        for (int j = 0; j < map.length; j++) {
-            for (int k = 0; k < map[j].length; k++) {
-                switch (map[j][k]) {
+        for (int i = 0; i < map.length; i++) {
+            for (int j = 0; j < map[j].length; j++) {
+                switch (map[i][j]) {
                     case 1:
                         System.out.print("O");
                         break;
@@ -33,15 +33,21 @@ public class Print {
     }
 
     public static void stageEndPrint(int stage, Classification classification) {
+
         System.out.println("");
         System.out.println("******************");
         System.out.println("*Stage " + stage + " Clear!!!*");
-        System.out.printf("*명령 입력 횟수: %2d*\n", classification.getCount());
+        System.out.printf("*명령 입력 횟수: %2d*%n", classification.getCount());
         System.out.println("******************");
         System.out.println("");
+
     }
 
     public static void gameEndPrint(){
+
+        System.out.println("");
+        System.out.println("전체 게임을 클리어 하셨습니다!");
+        System.out.println("축하드립니다!!");
 
     }
 
