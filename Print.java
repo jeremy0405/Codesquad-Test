cc6ee6709d7ac62d03738fab608172bf68492ff0
Jang HyungSuk <jeremy0405@naver.com>
2021-12-08 09:50:35 +0900

    시작안내문구에 u, U 관련 내용 추가
    한 일
     - u, U에 대한 사항을 게임 시작 전에 출력하는 print.gameStartMessage() 메서드에 내용을 추가함
    
    todoList
     - 리팩토링
     - U에 대한 자잘한 버그 수정

--- Print.java
+++ Print.java
@@ -47,15 +47,19 @@ public class Print {
     public static void gameStartMessage() {
 
         System.out.println("제리의 소코반에 오신 것을 환영합니다!!");
-        System.out.println("===============================================================");
-        System.out.println("**************************소코반 규칙****************************");
+        System.out.println("=================================================================");
+        System.out.println("**************************소코반 규칙*****************************");
         System.out.println("1. P(플레이어)를 w a s d 입력하여 움직일 수 있습니다.");
         System.out.println("2. P(플레이어)는 o(공)을 밀 수 있습니다.");
         System.out.println("   단! 공을 미는 방향에 #(벽) 또는 다른 공이 있으면 밀 수 없습니다.");
-        System.out.println("3. o(공)을 모두 O(구멍)에 밀어 넣으면 Stage 클리어!");
+        System.out.println("3. o(공)을 모두 O(구멍)에 밀어 넣으면 Stage 클리어! (총 5 Stage)");
         System.out.println("4. r 입력 시 Stage를 초기화 할 수 있습니다.");
         System.out.println("5. q 입력 시 게임을 종료할 수 있습니다.");
-        System.out.println("===============================================================");
+        System.out.println("6. u 입력 시 이전 입력을 되돌릴 수 있습니다.");
+        System.out.println("7. U 입력 시 u(이전 입력)을 되돌릴 수 있습니다.");
+        System.out.println("   단! U외에 이전 입력이 u일 경우만 가능합니다. (w a s d 이동시 U 불가)");
+        System.out.println("    사용 예시 ex) uuuu UU");
+        System.out.println("=================================================================");
 
     }
 
