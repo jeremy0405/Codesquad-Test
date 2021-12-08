f15866ede9e1bbbb78e1f9926122b51591458d1f
Jang HyungSuk <jeremy0405@naver.com>
2021-12-08 16:32:02 +0900

    루빅스큐브 Input 로직으로 변경테스트
    한 일
     - 1S, 2S 3S 등 명령어를 받기 위해
     - 루빅스큐브 input 로직을 가져와서 테스트하는 중
    
    todoList
     - Input 로직 변경

---
+++ NewUserInput.java
@@ -0,0 +1,71 @@
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Scanner;
+
+public class NewUserInput {
+
+    private final Scanner sc = new Scanner(System.in);
+
+    public String[] userInput() {
+        System.out.print("Cube> ");
+        String userinput = sc.nextLine();
+        String[] splitCommand = splitInput(userinput);
+        String[] commands = validateInput(splitCommand);
+        return commands;
+    }
+
+    private String[] validateInput(String[] command) {
+        for (int i = 0; i < command.length; i++) {
+            if (!command[i].equalsIgnoreCase("W") &&
+                !command[i].equalsIgnoreCase("A'") &&
+                !command[i].equalsIgnoreCase("S") &&
+                !command[i].equalsIgnoreCase("D") &&
+                !command[i].equalsIgnoreCase("Q") &&
+                !command[i].equalsIgnoreCase("U") &&
+                !command[i].equalsIgnoreCase("R") &&
+                !command[i].equals("1S") &&
+                !command[i].equals("2S") &&
+                !command[i].equals("3S") &&
+                !command[i].equals("4S") &&
+                !command[i].equals("5S") &&
+                !command[i].equals("1L") &&
+                !command[i].equals("2L") &&
+                !command[i].equals("3L") &&
+                !command[i].equals("4L") &&
+                !command[i].equals("5L")
+            ) {
+                System.out.println("잘못된 입력값이 포함되어 있습니다. 다시 입력해주세요!");
+                return userInput();
+            }
+        }
+        return command;
+    }
+
+    private String[] splitInput(String userinput) {
+        List<String> commandlist = new LinkedList<>();
+        for (int i = 0; i < userinput.length(); i++) {
+
+            if (i < userinput.length() - 1 && (userinput.charAt(i) == '1' || userinput.charAt(i) == '2'
+                || userinput.charAt(i) == '3' || userinput.charAt(i) == '4'
+                || userinput.charAt(i) == '5') && (userinput.charAt(i + 1) == 'S' || userinput.charAt(i + 1) == 'L')) {
+                int idx = commandlist.size();
+                commandlist.set(idx, commandlist.get(idx) + userinput.charAt(i + 1));
+                continue;
+            }
+            commandlist.add(userinput.charAt(i) + "");
+        }
+        return commandlist.toArray(new String[0]);
+    }
+
+    public void close() {
+        sc.close();
+    }
+
+    public static void main(String[] args) {
+        NewUserInput input = new NewUserInput();
+        for (var a: input.userInput()) {
+            System.out.println(a);
+        }
+    }
+
+}
